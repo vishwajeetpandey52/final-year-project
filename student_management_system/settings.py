421b45c6e8a563785ded17dc3dbdb08ad8fda886
vishwajeetpandey52 <60972325+vishwajeetpandey52@users.noreply.github.com>
2022-06-24 15:37:04 +0530

    Update settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","qwerty")  # Consider using your secret
 DEBUG = True
 
 # ALLOWED_HOSTS = ['smswithdjango.herokuapp.com']
-ALLOWED_HOSTS = []  # Not recommended but useful in dev mode
+ALLOWED_HOSTS = ['*']  # Not recommended but useful in dev mode
 
 
 # Application definition
